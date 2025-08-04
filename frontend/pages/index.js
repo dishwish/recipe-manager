dd80ed3b8afece0e3d37bac796faa2cc8686dc73
dishwish <julinwunderland@gmail.com>
2025-08-04 16:59:27 +0200

    Create index.js
    chore: add frontend home page

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>Recipe Manager</h1>
+      <p>Добро пожаловать в систему управления рецептами.</p>
+    </div>
+  )
+}
